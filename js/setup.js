429d8f94d043a92c7093a079881d4410c60d5b91
Natalia Ilina <metlinova86@gmail.com>
2017-11-29 15:57:19 +0300

    Добавлены обработчики событий в setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,50 @@
 'use strict';
 
 var TOTAL_WIZARDS_NUMBER = 4;
-var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARDS_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+var WIZARDS_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var WIZARDS_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var WIZARDS_COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var WIZARDS_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+var WIZARDS_FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
 
 var copyWizardsNames = WIZARDS_NAMES.slice();
 var copyWizardsSurnames = WIZARDS_SURNAMES.slice();
@@ -15,7 +55,23 @@ var wizardsListElement = document.querySelector('.setup-similar-list');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var fragment = document.createDocumentFragment();
 
-document.querySelector('.setup').classList.remove('hidden');
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var setupNameInput = setup.querySelector('.setup-user-name');
+var setupSubmit = setup.querySelector('.setup-submit');
+var mainWizard = setup.querySelector('.setup-wizard');
+var mainWizardCoat = mainWizard.querySelector('.wizard-coat');
+var mainWizardEyes = mainWizard.querySelector('.wizard-eyes');
+var mainWizardFireball = setup.querySelector('.setup-fireball-wrap');
+var coatInput = setup.querySelector('input[name=coat-color]');
+var eyesInput = setup.querySelector('input[name=eyes-color]');
+var fireballInput = setup.querySelector('input[name=fireball-color]');
+
+// открываем попап по клику/нажатию на иконку
+setupOpen.addEventListener('click', openPopup);
+setupOpen.addEventListener('keydown', onIconEnterPress);
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 // заполняем wizards
@@ -30,15 +86,158 @@ wizards.forEach(function (item, index) {
 // добавляем фрагмент в DOM
 addElement(wizardsListElement, fragment);
 
+// функции
 /**
-* getRandomElement - возвращает значение случайного элемента заданного массива, удаляя его из массива
+ * openPopup - показывает окно выбора персонажа
+ *
+ */
+function openPopup() {
+  setup.classList.remove('hidden');
+  // закрываем по esc
+  onEscClose();
+  // при фокусе на поле ввода отменяем закрытие по esc
+  setupNameInput.addEventListener('focus', removeOnEscClose);
+  setupNameInput.addEventListener('focusout', onEscClose);
+  // ловим клики на попапе
+  setup.addEventListener('click', onSetupClick);
+  // закрываем на кнопку/крестик по enter
+  setupClose.addEventListener('keydown', onButtonEnterPress);
+  setupSubmit.addEventListener('keydown', onButtonEnterPress);
+}
+
+/**
+ * closePopup - закрывает окно выбора персонажа
+ *
+ */
+function closePopup() {
+  setup.classList.add('hidden');
+  // удаляем обработчики
+  removeOnEscClose();
+  setupNameInput.removeEventListener('focus', removeOnEscClose);
+  setupNameInput.removeEventListener('focusout', onEscClose);
+  setup.removeEventListener('click', onSetupClick);
+  setupClose.removeEventListener('keydown', onButtonEnterPress);
+  setupSubmit.removeEventListener('keydown', onButtonEnterPress);
+}
+/**
+ * onWizardFireballClick - устанавливает значение цвета из массива в фон элемента и значение инпута
+ *
+ */
+function onWizardFireballClick() {
+  var fireball = getRandomElement(WIZARDS_FIREBALL_COLORS);
+  mainWizardFireball.style.backgroundColor = fireball;
+  fireballInput.value = fireball;
+}
+
+/**
+ * onWizardCoatClick - устанавливает значение цвета из массива в фон элемента и значение инпута
+ *
+ */
+function onWizardCoatClick() {
+  var coat = getRandomElement(WIZARDS_COAT_COLORS);
+  mainWizardCoat.style.fill = coat;
+  coatInput.value = coat;
+}
+
+/**
+ * onWizardEyesClick - устанавливает значение цвета из массива в фон элемента и значение инпута
+ *
+ */
+function onWizardEyesClick() {
+  var eyes = getRandomElement(WIZARDS_EYES_COLORS);
+  mainWizardEyes.style.fill = eyes;
+  eyesInput.value = eyes;
+}
+
+/**
+ * onPopupEscPress - закрывает окно при нажатии на esc
+ *
+ * @param  {Object} evt объект event
+ */
+function onPopupEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+}
+
+/**
+ * onEscClose - добавляет функцию закрытия окна по esc
+ *
+ */
+function onEscClose() {
+  document.addEventListener('keydown', onPopupEscPress);
+}
+
+/**
+ * removeOnEscClose - удаляет функцию закрытия окна по esc
+ *
+ */
+function removeOnEscClose() {
+  document.removeEventListener('keydown', onPopupEscPress);
+}
+
+/**
+ * onIconEnterPress - открывает окно при нажатии на enter при фокусе на иконке пользователя
+ *
+ * @param  {Object} evt объект event
+ */
+function onIconEnterPress(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+}
+
+/**
+ * onButtonEnterPress - закрывает окно при нажатии на enter при кнопке в фокусе
+ *
+ * @param  {Object} evt объект event
+ */
+function onButtonEnterPress(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+}
+
+
+/**
+ * onSetupClick - обрабатывает клики на открытом окне выбора персонажа
+ *
+ * @param  {Object} evt объект event
+ */
+function onSetupClick(evt) {
+  switch (evt.target) {
+    case mainWizardFireball.children[0]:
+      onWizardFireballClick();
+      break;
+    case mainWizardCoat:
+      onWizardCoatClick();
+      break;
+    case mainWizardEyes:
+      onWizardEyesClick();
+      break;
+    case setupSubmit:
+      closePopup();
+      break;
+    case setupClose:
+      closePopup();
+      break;
+  }
+}
+
+/**
+* getRandomElement - возвращает значение случайного элемента заданного массива
 *
 * @param {Array} arr массив
+* @param {boolean} noRepeat при значении true возвращает неповторяющиеся элементы
 * @return {string} случайный элемент массива
 */
-function getRandomElement(arr) {
+function getRandomElement(arr, noRepeat) {
   var index = Math.floor(Math.random() * arr.length);
-  return arr.splice(index, 1);
+  if (noRepeat) {
+    return arr.splice(index, 1);
+  } else {
+    return arr[index];
+  }
 }
 
 /**
@@ -58,9 +257,9 @@ function addElement(parentElement, childElement) {
  */
 function generateObject() {
   var obj = {};
-  obj.name = getRandomElement(copyWizardsNames) + ' ' + getRandomElement(copyWizardsSurnames);
-  obj.coatColor = getRandomElement(copyWizardsCoatColors);
-  obj.eyesColor = getRandomElement(copyWizardsEyesColors);
+  obj.name = getRandomElement(copyWizardsNames, true) + ' ' + getRandomElement(copyWizardsSurnames, true);
+  obj.coatColor = getRandomElement(copyWizardsCoatColors, true);
+  obj.eyesColor = getRandomElement(copyWizardsEyesColors, true);
   return obj;
 }
 
