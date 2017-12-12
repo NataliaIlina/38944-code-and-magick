3a068ab5e3d3f961f7975a8ba091bf7fb4a41fd9
Natalia Ilina <metlinova86@gmail.com>
2017-12-12 12:04:12 +0300

    Отрефакторены функции и их jsDoc в модуле util.js

--- js/util.js
+++ js/util.js
@@ -4,72 +4,78 @@
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
 
-  window.util = {
-    /**
-     * при истинности заданного условия вызывает функцию
-     *
-     * @param  {Event} evt Event
-     * @param  {function} action вызываемая функция
-     */
-    isEscPress: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
-        action();
-      }
-    },
+  /**
+   * при истинности заданного условия вызывает функцию
+   *
+   * @param  {Event} evt
+   * @param  {function} action
+   */
+  function isEscPress(evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
+  }
 
-    /**
-     * при истинности заданного условия вызывает функцию
-     *
-     * @param  {Event} evt Event
-     * @param  {function} action вызываемая функция
-     */
-    isEnterPress: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action();
-      }
-    },
+  /**
+   * при истинности заданного условия вызывает функцию
+   *
+   * @param  {Event} evt
+   * @param  {function} action
+   */
+  function isEnterPress(evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  }
 
-    /**
-    * Возвращает случайное число в заданном диапазоне
-    *
-    * @param {number} min минимальное значение
-    * @param {number} max максимальное значение
-    * @return {number} случайное число между min и max
-    */
-    getRandomNumber: function (min, max) {
-      return Math.random() * (max - min) + min;
-    },
+  /**
+   * Возвращает случайное число в заданном диапазоне
+   *
+   * @param {number} min минимальное значение
+   * @param {number} max максимальное значение
+   * @return {number} случайное число между min и max
+   */
+  function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-    /**
-    * Возвращает элемент массива с максимальным значением
-    *
-    * @param {array} arr массив чисел
-    * @return {number} элемент массива с максимальным значением
-    */
-    getMaxValue: function (arr) {
-      var maxValue = 0;
-      for (var i = 0; i < arr.length; i++) {
-        if (arr[i] > maxValue) {
-          maxValue = arr[i];
-        }
-      }
-      return maxValue;
-    },
-
-    /**
-    * getRandomElement - возвращает значение случайного элемента заданного массива
-    *
-    * @param {Array} arr массив
-    * @param {boolean} noRepeat при значении true возвращает неповторяющиеся элементы
-    * @return {string} случайный элемент массива
-    */
-    getRandomElement: function (arr, noRepeat) {
-      var index = Math.floor(Math.random() * arr.length);
-      if (noRepeat) {
-        return arr.splice(index, 1);
-      } else {
-        return arr[index];
+  /**
+   * Возвращает элемент массива с максимальным значением
+   *
+   * @param {array} arr массив чисел
+   * @return {number} элемент массива с максимальным значением
+   */
+  function getMaxValue(arr) {
+    var maxValue = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxValue) {
+        maxValue = arr[i];
       }
     }
+    return maxValue;
+  }
+
+  /**
+   * getRandomElement - возвращает значение случайного элемента заданного массива
+   *
+   * @param {Array} arr
+   * @param {boolean} [noRepeat] при значении true возвращает неповторяющиеся элементы
+   * @return {*} случайный элемент массива
+   */
+  function getRandomElement(arr, noRepeat) {
+    var index = Math.floor(Math.random() * arr.length);
+    if (noRepeat) {
+      return arr.splice(index, 1);
+    } else {
+      return arr[index];
+    }
+  }
+
+  window.util = {
+    isEscPress: isEscPress,
+    isEnterPress: isEnterPress,
+    getRandomNumber: getRandomNumber,
+    getMaxValue: getMaxValue,
+    getRandomElement: getRandomElement
   };
 })();
